17156224d7bafae18fb21914c4ac6ba929cfaca4
Leszek Rusinowicz <leszek.rusinowicz@futureelectronics.com>
2019-05-29 17:46:24 +0200

    Release version 1.2.0
    - Added support for noise level sensor.
    - Added Sequana informations characteristic.
    - Fixed a bug causing sometimes garbage readout for light sensor.
    - Calibrated RGB LEDs to better match requested color.

--- version/app_version.h
+++ version/app_version.h
@@ -4,8 +4,8 @@
 #ifndef _APP_VERSION_H_
 #define _APP_VERSION_H_
 
-#define APP_VERSION_STR     "eng_build"
-#define APP_VERSION_MAJOR   0
-#define APP_VERSION_MINOR   0
+#define APP_VERSION_STR     "1.2.0"
+#define APP_VERSION_MAJOR   1
+#define APP_VERSION_MINOR   2
 
 #endif // _APP_VERSION_H_
