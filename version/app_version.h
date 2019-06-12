0c49e5aa955d9e01c04e55e268285168d94c53e1
Leszek Rusinowicz <leszek.rusinowicz@futureelectronics.com>
2019-06-12 15:17:38 +0200

    Release version 1.3.0
    - Added support for PIR-based occupancy sensor.
    - Fixed a bug which could cause humidity and temperature updates
      to be skipped.

--- version/app_version.h
+++ version/app_version.h
@@ -4,8 +4,8 @@
 #ifndef _APP_VERSION_H_
 #define _APP_VERSION_H_
 
-#define APP_VERSION_STR     "1.2.0"
+#define APP_VERSION_STR     "1.3.0"
 #define APP_VERSION_MAJOR   1
-#define APP_VERSION_MINOR   2
+#define APP_VERSION_MINOR   3
 
 #endif // _APP_VERSION_H_
